7525a0e5bb0f5c17ce3824c2bb40e79448c5dc0d
Rishabh Sharma <rishabh2241991@gmail.com>
2021-04-09 21:18:30 +0530

    Update Google_Email_IDs_Scrapping_Tool.py

--- Google_Email_IDs_Scrapping_Tool.py
+++ Google_Email_IDs_Scrapping_Tool.py
@@ -1,4 +1,4 @@
-#python 3
+#Python3
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,8 +6,9 @@ import time
 from random import randint
 
 regex = r"([a-zA-Z0-9_.+-])+(@gmail.com)"
-i = 0
-while (i<50):
+i = 51
+data = []
+while (i<60):
         f = open("emaillist.txt","a")
         #Google Dork used here: [intext:"gmail.com" site:linkedin.com], for changing the page like in Google we chaged the page number, I used "i" variable for that.
         page = requests.get("https://www.google.com/search?q=intext:%22gmail.com%22+site:linkedin.com&sxsrf=ALeKk01cBkyaitS1ypoG5y5oPKU3SKPz4w:1617967532812&ei=rDlwYPGGMf-d4-EPvMu4wAU&start="+str(i)+"&sa=N&ved=2ahUKEwjxjLTghvHvAhX_zjgGH>
@@ -15,7 +16,6 @@ while (i<50):
         page_body = soup.body
         text = str(page_body)
         matches = re.finditer(regex, text, re.MULTILINE)
-        data = []
         for matchNum, match in enumerate(matches, start=1):
                 emails = ("{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
                 if emails not in data:
@@ -24,6 +24,8 @@ while (i<50):
                         print (filteremail)
                         f.write(filteremail)
                         f.write("\n")
+                else:
+                        print ("[-]"+emails+" exist in database....")
 
         i = i+1
         f.close()
